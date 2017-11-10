8e70e7789e506cdd9fdb074b9fa07cc4a7c208ef
surya <vs.mokkarala@stackroute.in>
2017-11-10 10:50:03 +0530

    test cases - test case for checking if data is saved or not

--- test/test.js
+++ test/test.js
@@ -1,1 +1,26 @@
-test.js
+let chai = require('chai');
+let should = require("chai").should();
+let server = require("../app.js");
+let assert = require("chai").assert;
+let request = require('supertest');
+
+let chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+// post names
+
+describe('post /names/save',()=>{
+  it('should save the names',(done)=>{
+    let name = {id:'10', Firstname:'surya', Lastname:'veera'}
+    chai.request(server)
+    .post('/names/save')
+    .send(name)
+    .end((err,res)=>{
+      should.not.exist(err);
+      res.status.should.equal(200);
+      res.body.should.be.a('object');
+      done();
+    });
+  });
+});
+
